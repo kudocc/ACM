0c97f9f3909c184ea66ee75ef4eebfbb5c96422c
yuanrui <yuanrui@yuanruitekiMacBook-Pro-2.local>
2015-02-03 10:42:01 +0800

    Initial Commit

---
+++ console/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  console
+//
+//  Created by yuanrui on 15-2-3.
+//  Copyright (c) 2015年 KudoCC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
